adb01a83dc3beb358e2050120c87b78d82a375a9
adityak994 <96017104+adityak994@users.noreply.github.com>
2022-08-14 14:30:46 +0530

    Add files via upload

---
+++ ArrayListNum.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ArrayListNum {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(23);
+        numbers.add(71);
+        numbers.add(47);
+        numbers.add(12);
+        System.out.println("Stored numbers are " + numbers);
+    }
+}
